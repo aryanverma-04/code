5cbd7f54a6b9712274ddd837d7136e87b2f02047
Aryan <aryanvetma062@gmail.com>
2022-12-28 21:14:16 +0530

    28dec

--- DSA Busted/LOVE/lec33.cpp
+++ DSA Busted/LOVE/lec33.cpp
@@ -1,8 +1,20 @@
-//! Checking if github is working or not
+//! RECURSION
 #include<bits/stdc++.h>
 using namespace std;
+int getSum(int arr[], int size){
+    if(size == 0){
+        return 0;
+    }
+    if(size == 1){
+        return arr[0];
+    }
+    int remainingpart = getSum(arr+1, size - 1);
+    int sum = arr[0] + remainingpart;
+    return sum;
+}
 int main()
 {
-    cout<<"HEloo world";
+    int arr[5] = {10, 20, 30 ,40 , 50};
+    cout<<getSum(arr, 5); 
     return 0;
 }
